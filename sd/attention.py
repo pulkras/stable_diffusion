ab28565d30b646b88c832d05f04f4cfe698d83b1
pulkras <maligin128@gmail.com>
2024-07-10 01:02:39 +0300

    attention function

---
+++ sd/attention.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import math
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, n_heads: int, d_embed: int, in_proj_bias = True, out_proj_bias=True): # embed = embedding in_proj_bias = in projection bias
+        super().__init__()
+
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+
+        self.out_proj == nn.Linear(d_embed, d_embed, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x:torch.Tensor, causal_mask=False):
+        # x = (Batch_Size, Seq_Len, Dim)
+
+        input_shape = x.shape
+
+        batch_size, sequence_length, d_embed = input_shape
+
+        interm_shape = (batch_size, sequence_length, self.n_heads, self.d_head) # intermideate shape
+
+        # q = query
+        # k = keys
+        # v = values
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim * 3) -> 3 tensors of shape (Batch_Size, Seq_Len, Dim)
+        q, k, v = self.in_proj(x).chunk(3, dim=-1)
+
+        # (Batch_Size, Seq_Len, Dim) -> (Batch_Size, Seq_Len, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
+        q = q.view(interm_shape).transpose(1, 2)
+        k = k.view(interm_shape).transpose(1, 2)
+        v = v.view(interm_shape).transpose(1, 2)
+
+        # (Batch_Size, H, Seq_Len, Seq_Len)
+        weight = q @ k.transpose(-1, -2)
+
+        if causal_mask:
+            # mask where the upper triangle (above the principal diagonal) is made up of 1
+            mask = torch.ones_like(weight, dtype=torch.bool).triu(1)
+            weight.masked_fill_(mask, -torch.inf)
+        
+        weight /= math.sqrt(self.d_head0)
+
+        weight = F.sotmax(weight, dim=-1)
+
+        # (Batch_Size, H, Seq_Len, Seq_Len) @ (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, H, Seq_Len, Dim / H)
+        output = weight @ v
+        # (Batch_Size, H, Seq_Len, Dim / H) -> (Batch_Size, Seq_Len, H, Dim / H)
+        output = output.transpose(1, 2)
+
+        output = output.reshape(input_shape)
+
+        output = self.out_proj(output)
+        
+        # (Batch_Size, Seq_Len, Dim)
+        return output
